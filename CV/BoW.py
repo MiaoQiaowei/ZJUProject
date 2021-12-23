97fa0e5c385a2ac4c33e4e804efb4e9b2f1b757c
MiaoQiaowei <252544058@qq.com>
2021-12-23 20:41:26 +0800

    完成CV作业

--- CV/BoW.py
+++ CV/BoW.py
@@ -23,75 +23,70 @@ class BoW():
         
     
     def save_bow(self, train_path):
-        training_names = os.listdir(train_path)
-        numWords = 10  # 聚类中心数
+        train_path_list = os.listdir(train_path)
+        # 聚类中心数, 设定为同Mnist样本数目一样
+        word_num = 10  
 
         image_paths = []  # 所有图片路径
-        ImageSet = {}
-        for name in training_names:
-            ls = os.listdir(train_path + "/" + name)
-            ImageSet[name] = len(ls)
-            for training_name in ls[:int(len(ls) / 3)]:
-                image_path = os.path.join(train_path + name, training_name)
+        dataset = {}
+        for set_name in train_path_list:
+            path_list = os.listdir(train_path + "/" + set_name)
+            dataset[set_name] = len(path_list)
+            for file_path in path_list:
+                image_path = os.path.join(train_path + set_name, file_path)
                 image_paths += [image_path]
 
         sift_det=cv2.xfeatures2d.SIFT_create()
-        des_list=[]  # 特征描述
+        descriptor_list=[]  # 特征描述
 
 
-        for name, num in ImageSet.items():
+        for name, num in dataset.items():
             dir = train_path + name
             print("从 " + name + " 中提取特征")
-            for filename in os.listdir(dir):
-                filename = f'{dir}//{filename}'
-                img=cv2.imread(filename)
+            for file_name in os.listdir(dir):
+                file_name = f'{dir}//{file_name}'
+                img=cv2.imread(file_name)
                 gray=cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
-                kp,des=sift_det.detectAndCompute(gray,None)
-                if des is None:
+                keypoint,descriptor=sift_det.detectAndCompute(gray,None)
+                if descriptor is None:
                     continue
-                des_list.append((image_path, des))
+                descriptor_list.append((image_path, descriptor))
 
-        descriptors = des_list[0][1]
-        print(descriptors.shape)
-        print(des_list[3][1].shape)
-        print('生成向量数组')
-        for image_path, descriptor in des_list[1:]:
-            # descriptors = np.vstack((descriptors, descriptor)) 
+        descriptors = descriptor_list[0][1]
+        for image_path, descriptor in descriptor_list[1:]:
             descriptors = np.concatenate([descriptors,descriptor],axis=0) 
 
-        print ("开始 k-means 聚类: %d words, %d key points" %(numWords, descriptors.shape[0]))
-        voc, variance = kmeans(descriptors, numWords, 1) 
+        print ("开始 k-means 聚类: %d words, %d key points" %(word_num, descriptors.shape[0]))
+        voc, variance = kmeans(descriptors, word_num, 1) 
 
-        im_features = np.zeros((len(image_paths), numWords), "float32")
+        image_features = np.zeros((len(image_paths), word_num), "float32")
         for i in range(len(image_paths)):
-            words, distance = vq(des_list[i][1],voc)
+            words, distance = vq(descriptor_list[i][1],voc)
             for w in words:
-                im_features[i][w] += 1
+                image_features[i][w] += 1
 
-        nbr_occurences = np.sum( (im_features > 0) * 1, axis = 0)
+        nbr_occurences = np.sum( (image_features > 0) * 1, axis = 0)
         idf = np.array(np.log((1.0*len(image_paths)+1) / (1.0*nbr_occurences + 1)), 'float32')
 
-        im_features = im_features*idf
-        im_features = preprocessing.normalize(im_features, norm='l2')
-
-        print('保存词袋模型文件')
-        joblib.dump((im_features, image_paths, idf, numWords, voc), "bow.pkl", compress=3)
+        image_features = image_features*idf
+        image_features = preprocessing.normalize(image_features, norm='l2')
+        joblib.dump((image_features, image_paths, idf, word_num, voc), "bow.pkl", compress=3)
     
     def predict(self, image_path):
         
-        im_features, image_paths, idf, numWords, voc = joblib.load("bow.pkl")
+        image_features, image_paths, idf, word_num, voc = joblib.load("bow.pkl")
             
         sift_det=cv2.xfeatures2d.SIFT_create()
-        des_list = []
+        descriptor_list = []
 
         im = cv2.imread(image_path)
         gray = cv2.cvtColor(im, cv2.COLOR_RGB2GRAY)
-        kp, des = sift_det.detectAndCompute(gray, None)
+        keypoint, descriptor = sift_det.detectAndCompute(gray, None)
 
-        des_list.append((image_path, des))   
-        descriptors = des_list[0][1]
+        descriptor_list.append((image_path, descriptor))   
+        descriptors = descriptor_list[0][1]
 
-        test_features = np.zeros((1, numWords), "float32")
+        test_features = np.zeros((1, word_num), "float32")
         if descriptors is None:
             return 0
         words, distance = vq(descriptors,voc)
@@ -101,7 +96,7 @@ class BoW():
         test_features = test_features*idf
         test_features = preprocessing.normalize(test_features, norm='l2')
 
-        score = np.dot(test_features, im_features.T)
+        score = np.dot(test_features, image_features.T)
         rank_ID = np.argsort(-score)
 
         for i in range(1):
@@ -112,22 +107,6 @@ class BoW():
             if top1_predict_image_label == label:
                 return 1
         return 0
-    
-    def show(self,im ,image_paths, rank_ID):
-        figure('基于OpenCV的图像检索')
-        subplot(5,5,1)#
-        title('目标图片')
-        imshow(im[:,:,::-1])
-        axis('off')
-        print(rank_ID)
-        for i, ID in enumerate(rank_ID[0][0:20]):
-            print(ID)
-            img = Image.open(image_paths[ID])
-            subplot(5,5,i+6)
-            imshow(img)
-            title('第%d相似'%(i+1))
-            axis('off')
-        show() 
 
 if __name__ =='__main__':
     bow = BoW(args)
